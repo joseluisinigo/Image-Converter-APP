066b11faf1428f47615abf1a93207e982a578d09
Jose luis iñigo A.K.A. Riskoo <joseluisib@gmail.com>
2025-01-18 11:05:45 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace ImageConverterApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        // Declaración de controles
+        internal System.Windows.Forms.ListBox lstImagenes;
+        internal System.Windows.Forms.Button btnAgregar;
+        internal System.Windows.Forms.Button btnEliminar;
+        internal System.Windows.Forms.TextBox txtAncho;
+        internal System.Windows.Forms.TextBox txtAlto;
+        internal System.Windows.Forms.Label lblAncho;
+        internal System.Windows.Forms.Label lblAlto;
+        internal System.Windows.Forms.CheckBox chkMantenerAspectRatio;
+        internal System.Windows.Forms.ComboBox cmbFormato;
+        internal System.Windows.Forms.Button btnConvertir;
+        internal System.Windows.Forms.Label lblUnidad;
+        internal System.Windows.Forms.ComboBox cmbUnidad; // Para seleccionar "px", "cm" o "mm"
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No modifique el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstImagenes = new System.Windows.Forms.ListBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.txtAncho = new System.Windows.Forms.TextBox();
+            this.txtAlto = new System.Windows.Forms.TextBox();
+            this.lblAncho = new System.Windows.Forms.Label();
+            this.lblAlto = new System.Windows.Forms.Label();
+            this.chkMantenerAspectRatio = new System.Windows.Forms.CheckBox();
+            this.cmbFormato = new System.Windows.Forms.ComboBox();
+            this.btnConvertir = new System.Windows.Forms.Button();
+            this.lblUnidad = new System.Windows.Forms.Label();
+            this.cmbUnidad = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // lstImagenes
+            // 
+            this.lstImagenes.AllowDrop = true;
+            this.lstImagenes.FormattingEnabled = true;
+            this.lstImagenes.ItemHeight = 15;
+            this.lstImagenes.Location = new System.Drawing.Point(20, 20);
+            this.lstImagenes.Name = "lstImagenes";
+            this.lstImagenes.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.lstImagenes.Size = new System.Drawing.Size(300, 200);
+            this.lstImagenes.TabIndex = 0;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(340, 20);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(240, 30);
+            this.btnAgregar.TabIndex = 1;
+            this.btnAgregar.Text = "Add Image / Agregar Imagen";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(340, 60);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(240, 30);
+            this.btnEliminar.TabIndex = 2;
+            this.btnEliminar.Text = "Remove Image / Eliminar Imagen";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // txtAncho
+            // 
+            this.txtAncho.Location = new System.Drawing.Point(130, 237);
+            this.txtAncho.Name = "txtAncho";
+            this.txtAncho.Size = new System.Drawing.Size(100, 23);
+            this.txtAncho.TabIndex = 3;
+            // 
+            // txtAlto
+            // 
+            this.txtAlto.Location = new System.Drawing.Point(130, 277);
+            this.txtAlto.Name = "txtAlto";
+            this.txtAlto.Size = new System.Drawing.Size(100, 23);
+            this.txtAlto.TabIndex = 4;
+            // 
+            // lblAncho
+            // 
+            this.lblAncho.AutoSize = true;
+            this.lblAncho.Location = new System.Drawing.Point(20, 240);
+            this.lblAncho.Name = "lblAncho";
+            this.lblAncho.Size = new System.Drawing.Size(89, 15);
+            this.lblAncho.TabIndex = 5;
+            this.lblAncho.Text = "Width / Ancho:";
+            // 
+            // lblAlto
+            // 
+            this.lblAlto.AutoSize = true;
+            this.lblAlto.Location = new System.Drawing.Point(20, 280);
+            this.lblAlto.Name = "lblAlto";
+            this.lblAlto.Size = new System.Drawing.Size(88, 15);
+            this.lblAlto.TabIndex = 6;
+            this.lblAlto.Text = "Height / Alto:";
+            // 
+            // chkMantenerAspectRatio
+            // 
+            this.chkMantenerAspectRatio.AutoSize = true;
+            this.chkMantenerAspectRatio.Location = new System.Drawing.Point(20, 320);
+            this.chkMantenerAspectRatio.Name = "chkMantenerAspectRatio";
+            this.chkMantenerAspectRatio.Size = new System.Drawing.Size(245, 19);
+            this.chkMantenerAspectRatio.TabIndex = 7;
+            this.chkMantenerAspectRatio.Text = "Maintain Aspect Ratio / Mantener Aspect Ratio";
+            this.chkMantenerAspectRatio.UseVisualStyleBackColor = true;
+            // 
+            // cmbFormato
+            // 
+            this.cmbFormato.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbFormato.FormattingEnabled = true;
+            this.cmbFormato.Items.AddRange(new object[] {
+            "JPG",
+            "PNG",
+            "WEBP",
+            "GIF",
+            "BMP",
+            "TIFF",
+            "ICO",
+            "PDF"});
+            this.cmbFormato.Location = new System.Drawing.Point(20, 360);
+            this.cmbFormato.Name = "cmbFormato";
+            this.cmbFormato.Size = new System.Drawing.Size(150, 23);
+            this.cmbFormato.TabIndex = 8;
+            // 
+            // lblUnidad
+            // 
+            this.lblUnidad.AutoSize = true;
+            this.lblUnidad.Location = new System.Drawing.Point(20, 400);
+            this.lblUnidad.Name = "lblUnidad";
+            this.lblUnidad.Size = new System.Drawing.Size(171, 15);
+            this.lblUnidad.TabIndex = 10;
+            this.lblUnidad.Text = "Measurement Unit / Unidad:";
+            // 
+            // cmbUnidad
+            // 
+            this.cmbUnidad.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbUnidad.FormattingEnabled = true;
+            this.cmbUnidad.Items.AddRange(new object[] {
+            "px",
+            "cm",
+            "mm"});
+            this.cmbUnidad.Location = new System.Drawing.Point(200, 397);
+            this.cmbUnidad.Name = "cmbUnidad";
+            this.cmbUnidad.Size = new System.Drawing.Size(120, 23);
+            this.cmbUnidad.TabIndex = 11;
+            this.cmbUnidad.SelectedIndex = 0;
+            // 
+            // btnConvertir
+            // 
+            this.btnConvertir.Location = new System.Drawing.Point(340, 360);
+            this.btnConvertir.Name = "btnConvertir";
+            this.btnConvertir.Size = new System.Drawing.Size(240, 30);
+            this.btnConvertir.TabIndex = 9;
+            this.btnConvertir.Text = "Convert Images / Convertir Imágenes";
+            this.btnConvertir.UseVisualStyleBackColor = true;
+            this.btnConvertir.Click += new System.EventHandler(this.btnConvertir_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(600, 500);
+            this.Controls.Add(this.cmbUnidad);
+            this.Controls.Add(this.lblUnidad);
+            this.Controls.Add(this.btnConvertir);
+            this.Controls.Add(this.cmbFormato);
+            this.Controls.Add(this.chkMantenerAspectRatio);
+            this.Controls.Add(this.lblAlto);
+            this.Controls.Add(this.lblAncho);
+            this.Controls.Add(this.txtAlto);
+            this.Controls.Add(this.txtAncho);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.lstImagenes);
+            this.Name = "Form1";
+            this.Text = "Image Converter App / Convertidor de Imágenes";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using ImageMagick;
+
+namespace ImageConverterApp
+{
+    public partial class Form1 : Form
+    {
+        // La lista de imágenes ya se declara aquí para que sea accesible en toda la clase
+        private List<string> listaImagenes = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Asociar eventos de Drag & Drop en caso de no haberlo hecho en el Designer
+            lstImagenes.DragEnter += LstImagenes_DragEnter;
+            lstImagenes.DragDrop += LstImagenes_DragDrop;
+        }
+
+        #region Drag & Drop
+
+        private void LstImagenes_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+        }
+
+        private void LstImagenes_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (var file in files)
+            {
+                if (IsValidImage(file) && !listaImagenes.Contains(file))
+                {
+                    listaImagenes.Add(file);
+                    lstImagenes.Items.Add(Path.GetFileName(file));
+                }
+            }
+        }
+
+        #endregion
+
+        // Valida si el archivo tiene una extensión válida para imágenes
+        private bool IsValidImage(string path)
+        {
+            string ext = Path.GetExtension(path).ToLower();
+            return ext == ".jpg" || ext == ".jpeg" || ext == ".png" ||
+                   ext == ".webp" || ext == ".gif" || ext == ".bmp" ||
+                   ext == ".tif" || ext == ".tiff" || ext == ".ico" || ext == ".pdf";
+        }
+
+        // Botón: Agregar Imagen
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.Multiselect = true;
+                dlg.Filter = "Images|*.jpg;*.jpeg;*.png;*.webp;*.gif;*.bmp;*.tif;*.tiff;*.ico;*.pdf";
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    foreach (string file in dlg.FileNames)
+                    {
+                        if (IsValidImage(file) && !listaImagenes.Contains(file))
+                        {
+                            listaImagenes.Add(file);
+                            lstImagenes.Items.Add(Path.GetFileName(file));
+                        }
+                    }
+                }
+            }
+        }
+
+        // Botón: Eliminar Imagen (ahora permite eliminar varios elementos seleccionados)
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (lstImagenes.SelectedItems.Count > 0)
+            {
+                // Crear una lista temporal de los elementos seleccionados
+                var itemsToRemove = new List<string>();
+                foreach (var item in lstImagenes.SelectedItems)
+                {
+                    itemsToRemove.Add(item.ToString());
+                }
+                // Para cada elemento seleccionado, quitarlo de la lista y del ListBox
+                foreach (var item in itemsToRemove)
+                {
+                    // Obtener la ruta original correspondiente
+                    int index = lstImagenes.Items.IndexOf(item);
+                    if (index >= 0)
+                    {
+                        lstImagenes.Items.RemoveAt(index);
+                        listaImagenes.RemoveAt(index);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select one or more images to remove / Selecciona una o más imágenes para eliminar",
+                                "Warning / Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        // Botón: Convertir Imágenes
+        private void btnConvertir_Click(object sender, EventArgs e)
+        {
+            if (listaImagenes.Count == 0)
+            {
+                MessageBox.Show("Please add at least one image / Agrega al menos una imagen.",
+                                "Warning / Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            using (FolderBrowserDialog folderDlg = new FolderBrowserDialog())
+            {
+                if (folderDlg.ShowDialog() == DialogResult.OK)
+                {
+                    foreach (string path in listaImagenes)
+                    {
+                        try
+                        {
+                            ResizeAndConvertImage(path, folderDlg.SelectedPath);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error processing " + Path.GetFileName(path) + ": " + ex.Message);
+                        }
+                    }
+                    MessageBox.Show("Conversion completed / Conversión completada.",
+                                    "Success / Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Redimensiona y convierte la imagen desde la ruta de origen a la carpeta de destino,
+        /// utilizando los valores de ancho/alto y el formato especificado por el usuario.
+        /// Se utiliza Magick.NET para soportar múltiples formatos.
+        /// </summary>
+        private void ResizeAndConvertImage(string sourcePath, string destFolder)
+        {
+            using (var image = new MagickImage(sourcePath))
+            {
+                // Convertir propiedades de tipo uint a int
+                int originalWidth = (int)image.Width;
+                int originalHeight = (int)image.Height;
+                int newWidth = 0, newHeight = 0;
+
+                // Leer los valores ingresados para ancho y alto
+                int.TryParse(txtAncho.Text, out newWidth);
+                int.TryParse(txtAlto.Text, out newHeight);
+
+                // Obtener la unidad de medida (px, cm o mm) seleccionada
+                string unidad = cmbUnidad.SelectedItem.ToString().ToLower();
+                // Factor de conversión de la unidad ingresada a píxeles (usando 96 DPI)
+                double factor = 1.0;
+                if (unidad == "cm")
+                    factor = 96.0 / 2.54; // Aproximadamente 37.7953
+                else if (unidad == "mm")
+                    factor = 96.0 / 25.4; // Aproximadamente 3.77953
+
+                // Convertir las medidas ingresadas a píxeles (si se han ingresado)
+                if (newWidth > 0) newWidth = (int)(newWidth * factor);
+                if (newHeight > 0) newHeight = (int)(newHeight * factor);
+
+                // Si "Mantener Aspect Ratio" está activado, calcular la dimensión faltante
+                if (chkMantenerAspectRatio.Checked)
+                {
+                    if (newWidth > 0 && newHeight == 0)
+                    {
+                        newHeight = (int)((double)newWidth * originalHeight / originalWidth);
+                    }
+                    else if (newHeight > 0 && newWidth == 0)
+                    {
+                        newWidth = (int)((double)newHeight * originalWidth / originalHeight);
+                    }
+                    else if (newWidth == 0 && newHeight == 0)
+                    {
+                        newWidth = originalWidth;
+                        newHeight = originalHeight;
+                    }
+                }
+                else
+                {
+                    if (newWidth == 0)
+                        newWidth = originalWidth;
+                    if (newHeight == 0)
+                        newHeight = originalHeight;
+                }
+
+                // Redimensionar la imagen (se requiere uint)
+                image.Resize((uint)newWidth, (uint)newHeight);
+
+                // Determinar el formato de salida y la extensión correspondiente
+                string selectedFormat = cmbFormato.SelectedItem.ToString().ToUpper();
+                MagickFormat outputFormat;
+                string ext;
+                switch (selectedFormat)
+                {
+                    case "JPG":
+                    case "JPEG":
+                        outputFormat = MagickFormat.Jpeg;
+                        ext = ".jpg";
+                        break;
+                    case "PNG":
+                        outputFormat = MagickFormat.Png;
+                        ext = ".png";
+                        break;
+                    case "WEBP":
+                        outputFormat = MagickFormat.WebP;
+                        ext = ".webp";
+                        break;
+                    case "GIF":
+                        outputFormat = MagickFormat.Gif;
+                        ext = ".gif";
+                        break;
+                    case "BMP":
+                        outputFormat = MagickFormat.Bmp;
+                        ext = ".bmp";
+                        break;
+                    case "TIFF":
+                        outputFormat = MagickFormat.Tiff;
+                        ext = ".tiff";
+                        break;
+                    case "ICO":
+                        outputFormat = MagickFormat.Ico;
+                        ext = ".ico";
+                        break;
+                    case "PDF":
+                        outputFormat = MagickFormat.Pdf;
+                        ext = ".pdf";
+                        break;
+                    default:
+                        outputFormat = MagickFormat.Png;
+                        ext = ".png";
+                        break;
+                }
+                image.Format = outputFormat;
+                string outputFileName = System.IO.Path.GetFileNameWithoutExtension(sourcePath) + "_conv" + ext;
+                string destPath = System.IO.Path.Combine(destFolder, outputFileName);
+                image.Write(destPath);
+            }
+        }
+    }
+}
